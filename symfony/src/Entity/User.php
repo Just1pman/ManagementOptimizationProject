f7ee2592c479fcb74e266883f16e396db9cc9535
Just1pman <Just1pman@gmail.com>
2021-01-09 15:47:38 +0300

    Added registration function

--- symfony/src/Entity/User.php
+++ symfony/src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -56,12 +58,23 @@ class User implements UserInterface
      */
     private $careerSummaries;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
+
     public function __construct()
     {
         $this->education = new ArrayCollection();
         $this->dateStart = new ArrayCollection();
         $this->careerSummaries = new ArrayCollection();
     }
+
+    public function __toString():string
+    {
+        return $this->email;
+    }
+
 
     public function getId(): ?int
     {
@@ -231,6 +244,18 @@ class User implements UserInterface
                 $careerSummary->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
